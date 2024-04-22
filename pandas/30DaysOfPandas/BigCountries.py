7be2b8d7b4d6c3703846abcbe9208d85e38291b9
Thomas <145370011+Krxnkos@users.noreply.github.com>
2024-04-22 20:33:47 +0100

    Update BigCountries.py

--- pandas/30DaysOfPandas/BigCountries.py
+++ pandas/30DaysOfPandas/BigCountries.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def bigCountries(world: pd.DataFrame) -> pd.DataFrame:
-    bigCountriesDf = world[(world['area'] >= 3000000) | (world    ['population'] >= 25000000)] #? | Determines whether the area has 3000000 km^2 or more
-    return bigCountriesDf[['name', 'population', 'area']] #? | Returns the "name", "population" & "area" columns
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    big_countries_df = world[(world['area'] >= 3000000) | (world    ['population'] >= 25000000)] #? | Determines whether the area has 3000000 km^2 or more
+    return big_countries_df[['name', 'population', 'area']] #? | Returns the "name", "population" & "area" columns
 
 ##################
 # Status: SOLVED #
